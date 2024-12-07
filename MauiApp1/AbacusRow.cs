69feb2b3b7422ecb4a4fb7a0249266f1a479c782
Monika <mon.fur89@gmail.com>
2024-12-07 01:32:52 +0100

    kész

---
+++ MauiApp1/AbacusRow.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace MauiApp1
+{
+    public class AbacusRow : INotifyPropertyChanged
+    {
+        private int _value;
+        private int _placeValue;
+
+        // A Slider változása automatikusan frissíti a Label-t
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));  // Frissíti a megfelelő PropertyChanged eseményt
+                }
+            }
+        }
+
+        public int PlaceValue
+        {
+            get { return _placeValue; }
+            set
+            {
+                if (_placeValue != value)
+                {
+                    _placeValue = value;
+                    OnPropertyChanged(nameof(PlaceValue));  // Frissíti a megfelelő PropertyChanged eseményt
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,25 +1,60 @@
-﻿namespace MauiApp1
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        // Az abakusz sorokat tároló lista
+        private List<AbacusRow> abacusRows;
+
+        // Command properties
+        public ICommand AddRowCommand { get; }
+        public ICommand SumCommand { get; }
 
         public MainPage()
         {
             InitializeComponent();
+            abacusRows = new List<AbacusRow>();
+            AddRow();  // Kezdetben hozzáadunk egy sort
+            AbacusCollectionView.ItemsSource = abacusRows;  // Beállítjuk a CollectionView forrást
+
+            // Inicializáljuk a parancsokat
+            AddRowCommand = new Command(OnAddRowClicked);
+            SumCommand = new Command(OnSumClicked);
+
+            // A BindingContext beállítása
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        // Abakusz sor hozzáadása
+        private void AddRow()
         {
-            count++;
+            // A következő helyi értéket számítjuk ki (az első sor helyi értéke 1, minden következő 10-szerese)
+            int placeValue = (abacusRows.Count == 0) ? 1 : abacusRows.Last().PlaceValue * 10;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            // Új AbacusRow hozzáadása a listához
+            abacusRows.Add(new AbacusRow { PlaceValue = placeValue, Value = 0 });
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Az új sor hozzáadása után automatikusan frissíteni kell a CollectionView-t
+            AbacusCollectionView.ItemsSource = null;
+            AbacusCollectionView.ItemsSource = abacusRows;
+        }
+
+        // Összegzés
+        private void OnSumClicked()
+        {
+            int sum = abacusRows.Sum(row => row.Value * row.PlaceValue);
+            TotalLabel.Text = sum.ToString();
+        }
+
+        // Új sor gomb kezelése
+        private void OnAddRowClicked()
+        {
+            AddRow();
         }
     }
-
 }
